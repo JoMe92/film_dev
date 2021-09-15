a5d671d85838be8028dbf6959f3acfdf00c1df82
Jonas Meier <jonasmeier@posteo.de>
2021-09-15 21:22:51 +0200

    add init.py files

--- test/develop_img.py
+++ test/develop_img.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-import src.core
+import test
+# from ..src import core
+import film_dev.src.core
+
+
 
 img_neg = cv2.imread(r"C:\Users\jonas\Development\Film_Developing\Test.tiff",-1) # load a 16-bit negative image
 img_neg = src.core.scale_percent(img_neg,10) # downscale to 50% of the image size
@@ -9,7 +13,7 @@ img_pos = src.core.invert(img_lev) # invert the leveled image
 # img_gamma = src.core.gamma(img_pos,3)
 
 # save the image as 8 bit jpg file
-cv2.imwrite("files\\img_neg.jpg", src.core.img16to8(img_neg))
+cv2.imwrite("files\\img_neg-t.jpg", src.core.img16to8(img_neg))
 cv2.imwrite("files\\img_lev.jpg", src.core.img16to8(img_lev))
 cv2.imwrite("files\\img_pos.jpg", src.core.img16to8(img_pos))
 # cv2.imwrite("img_gamma.jpg", src.core.img16to8(img_gamma))
